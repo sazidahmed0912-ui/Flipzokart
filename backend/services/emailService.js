7a2b979000fc37957fa606bef93c18d26716b7b0
Sazid Ahmed <sazidahmed0912@gmail.com>
2026-01-29 14:16:13 +0530

    fix email timeout use explicit smtp.zoho.in

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -1,7 +1,9 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: "Zoho",
+  host: "smtp.zoho.in",
+  port: 465,
+  secure: true,
   auth: {
     type: "OAuth2",
     user: process.env.ZOHO_MAIL,
